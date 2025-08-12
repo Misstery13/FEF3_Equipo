153c00c8e9f169561e42cac9fba28b01f74c2f7e
Diana <97330247+Misstery13@users.noreply.github.com>
2025-08-11 23:21:38 -0500

    Some updates for main menu

--- src/main/java/tarea3/fef3/FXMLMainController.java
+++ src/main/java/tarea3/fef3/FXMLMainController.java
@@ -2,12 +2,77 @@ package tarea3.fef3;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 
 public class FXMLMainController {
-    @javafx.fxml.FXML
-    private Button btn_guardar;
 
     @javafx.fxml.FXML
+    private Button btn_salir;
+    @javafx.fxml.FXML
+    private MenuItem btn_repersonal;
+    @javafx.fxml.FXML
+    private Menu btn_personal;
+    @javafx.fxml.FXML
+    private MenuItem btn_about;
+    @javafx.fxml.FXML
+    private MenuItem btn_requipo;
+    @javafx.fxml.FXML
+    private MenuItem btn_nomina;
+    @javafx.fxml.FXML
+    private MenuItem btn_regequipo;
+    @javafx.fxml.FXML
+    private Menu btn_equipo;
+    @javafx.fxml.FXML
+    private MenuItem btn_redirect;
+    @javafx.fxml.FXML
+    private MenuBar bar_menu;
+    @javafx.fxml.FXML
+    private MenuItem btn_regjuga;
+
+    @Deprecated
     public void acc_btnguardar(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acc_btnsalir(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acc_regequipo(ActionEvent actionEvent) {
+
+    }
+
+    @javafx.fxml.FXML
+    public void acc_about(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acc_redirect(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acc_repersonal(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acc_btnequipo(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acc_requipo(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acc_personal(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acc_regjuga(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acc_nomina(ActionEvent actionEvent) {
     }
 }
